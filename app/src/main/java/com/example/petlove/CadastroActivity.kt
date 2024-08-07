b3ab5792278f006bfc51c3e08923055d46269d2e
Ferreira96 <ferreira96ms@gmail.com>
2024-08-06 23:15:31 -0300

    Firebase Autenticação - correção

--- app/src/main/java/com/example/petlove/CadastroActivity.kt
+++ app/src/main/java/com/example/petlove/CadastroActivity.kt
@@ -1,4 +1,56 @@
 package com.example.petlove
 
-class CadastroActivity {
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class CadastroActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        auth = Firebase.auth/**AUTENTICAÇÃO*/
+        var lb_cad_email  = findViewById<EditText>(R.id.lb_cad_email)
+        var lb_cad_senha  = findViewById<EditText>(R.id.lb_cad_senha)
+
+        //AÇÃO DO BOTÃO [CADASTRO]
+        val btCadastro: Button = findViewById(R.id.bt_cadastro)
+        btCadastro.setOnClickListener {
+
+            val cad_email = lb_cad_email.text.toString()
+            val cad_senha = lb_cad_senha.text.toString()
+
+            if(!cad_email.equals("") or !cad_senha.equals("")){
+                createUserComEmailESenha(cad_email, cad_senha)
+            }else{
+                Toast.makeText(baseContext, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    }
+
+    /**AUTENTICAÇÃO*/
+    private fun createUserComEmailESenha(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ task->
+            if(task.isSuccessful){
+                Log.d("EmailAndPassword","createUserComEmailESenha-Sucesso")
+                val user = auth.currentUser
+            }else{
+                Log.d("EmailAndPassword","createUserComEmailESenha-Falha", task.exception)
+                Toast.makeText(baseContext, "Falha na autenticação", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/petlove/LoginActivity.kt
+++ app/src/main/java/com/example/petlove/LoginActivity.kt
@@ -2,27 +2,75 @@ package com.example.petlove
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth /**AUTENTICAÇÃO*/
+    private var isLogged = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //DEFINE XML
         setContentView(R.layout.activity_login)
 
+        auth = Firebase.auth/**AUTENTICAÇÃO*/
+        var lb_usuario  = findViewById<EditText>(R.id.lb_usuario)
+        var lb_senha  = findViewById<EditText>(R.id.lb_senha)
+
         //MANIPULA TX_TITULO
         var tx_titulo  = findViewById<TextView>(R.id.tx_titulo)
         tx_titulo.setText("-  PETLOVE  -")
 
-        //AÇÃO DO BOTÃO [LOGIN]
+        //AÇÃO DO BOTÃO [ENTRAR]
         val btEntrar: Button = findViewById(R.id.bt_entrar)
         btEntrar.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
+            var lb_usuario_login = lb_usuario.text.toString()
+            var lb_senha_login = lb_senha.text.toString()
+
+            if(!lb_usuario_login.equals("") or !lb_senha_login.equals("")){
+                signInWithUserComEmailESenha(lb_usuario_login, lb_senha_login)
+                if(isLogged){
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+            }else{
+                Toast.makeText(baseContext, "email ou senha invalidos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //AÇÃO DO TEXTVIEW [CADASTRO]
+        val btCadastro: TextView = findViewById(R.id.bt_cadastro)
+        btCadastro.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    /**LOGIN*/
+    private fun signInWithUserComEmailESenha(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task->
+            if(task.isSuccessful){
+                isLogged = true
+                Log.d("EmailAndPassword","signInUserComEmailESenha-Sucesso")
+                Toast.makeText(baseContext, "Login realizado com sucesso", Toast.LENGTH_SHORT).show()
+
+
+                //val user = auth.currentUser
+            }else{
+                Log.d("EmailAndPassword","signInUserComEmailESenha-Falha", task.exception)
+                Toast.makeText(baseContext, "email ou senha invalidos", Toast.LENGTH_SHORT).show()
+                isLogged = false
+            }
         }
     }
 }
